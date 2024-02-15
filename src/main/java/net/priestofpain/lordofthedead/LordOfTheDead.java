66d955d831954fa9a3022afc8ea7215afb36e0cf
PriestOfPain <PriestOfPainWasTaken@gmail.com>
2024-02-15 03:33:16 -0800

    fix: fix mod id naming convention

--- src/main/java/net/priestofpain/lordofthedead/LordOfTheDead.java
+++ src/main/java/net/priestofpain/lordofthedead/LordOfTheDead.java
@@ -20,7 +20,7 @@ import org.slf4j.Logger;
 public class LordOfTheDead
 {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "lord-of-the-dead";
+    public static final String MOD_ID = "lord_of_the_dead";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
