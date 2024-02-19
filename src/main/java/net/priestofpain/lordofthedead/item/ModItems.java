0fba8a797af3dc482af9d51b2718f19100b31dc1
PriestOfPain <PriestOfPainWasTaken@gmail.com>
2024-02-18 16:31:30 -0800

    feat: add barebones test item

--- src/main/java/net/priestofpain/lordofthedead/item/ModItems.java
+++ src/main/java/net/priestofpain/lordofthedead/item/ModItems.java
@@ -4,12 +4,16 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import net.priestofpain.lordofthedead.LordOfTheDead;
 
 public class ModItems {
     // Adds mod items to be registered when forge loads
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, LordOfTheDead.MOD_ID);
+
+    public static final RegistryObject<Item> NECROMANCER_STAFF = ITEMS.register("necromancer staff",
+            () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
