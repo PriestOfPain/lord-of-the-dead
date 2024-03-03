6a937a25b52388d3dd6cdd285e06d886ef3534bf
PriestOfPain <PriestOfPainWasTaken@gmail.com>
2024-03-02 17:03:43 -0800

    feat: add custom zombie class, add custom goal ai

---
+++ src/main/java/net/priestofpain/lordofthedead/entity/ModEntities.java
@@ -0,0 +1,4 @@
+package net.priestofpain.lordofthedead.entity;
+
+public class ModEntities {
+}
---
+++ src/main/java/net/priestofpain/lordofthedead/entity/ai/goal/FollowSummonerGoal.java
@@ -0,0 +1,10 @@
+package net.priestofpain.lordofthedead.entity.ai.goal;
+
+import net.minecraft.world.entity.ai.goal.Goal;
+
+public class FollowSummonerGoal extends Goal {
+    @Override
+    public boolean canUse() {
+        return false;
+    }
+}
