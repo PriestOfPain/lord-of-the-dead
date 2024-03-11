e7db148d9247fcaeef90f970515e96aab6a0d72b
PriestOfPain <PriestOfPainWasTaken@gmail.com>
2024-03-10 17:57:15 -0700

    feat: add simpleItem generation

--- src/main/java/net/priestofpain/lordofthedead/datagen/ModItemModelProvider.java
+++ src/main/java/net/priestofpain/lordofthedead/datagen/ModItemModelProvider.java
@@ -1,4 +1,28 @@
 package net.priestofpain.lordofthedead.datagen;
 
-public class ModItemModelProvider {
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+import net.priestofpain.lordofthedead.LordOfTheDead;
+import net.priestofpain.lordofthedead.item.ModItems;
+
+public class ModItemModelProvider extends ItemModelProvider {
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, LordOfTheDead.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        simpleItem(ModItems.NECROMANCER_STAFF);
+    }
+    // Generates our mod items
+    private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation(LordOfTheDead.MOD_ID, "item/" + item.getId().getPath()));
+    }
 }
