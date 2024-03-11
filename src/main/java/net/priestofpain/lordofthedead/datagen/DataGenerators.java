3d4e1266defe6acee10507e52ad0a2902a2fad16
PriestOfPain <PriestOfPainWasTaken@gmail.com>
2024-03-10 18:30:13 -0700

    feat: add datagenerator events

--- src/main/java/net/priestofpain/lordofthedead/datagen/DataGenerators.java
+++ src/main/java/net/priestofpain/lordofthedead/datagen/DataGenerators.java
@@ -1,4 +1,27 @@
 package net.priestofpain.lordofthedead.datagen;
 
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.priestofpain.lordofthedead.LordOfTheDead;
+
+import java.util.concurrent.CompletableFuture;
+
+@Mod.EventBusSubscriber(modid = LordOfTheDead.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataGenerators {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        // CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+        generator.addProvider(event.includeServer(), new ModRecipeProvider(packOutput));
+
+        generator.addProvider(event.includeServer(), new ModItemModelProvider(packOutput, existingFileHelper));
+    }
 }
