104f68dd63ba9a44dacf535b1a83334a0f4ea549
PriestOfPain <PriestOfPainWasTaken@gmail.com>
2024-03-12 16:55:25 -0700

    feat: add custom bone pile placeholder block

--- src/main/java/net/priestofpain/lordofthedead/datagen/ModBlockStateProvider.java
+++ src/main/java/net/priestofpain/lordofthedead/datagen/ModBlockStateProvider.java
@@ -6,6 +6,7 @@ import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.RegistryObject;
 import net.priestofpain.lordofthedead.LordOfTheDead;
+import net.priestofpain.lordofthedead.block.ModBlocks;
 
 public class ModBlockStateProvider extends BlockStateProvider {
     public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
@@ -14,10 +15,7 @@ public class ModBlockStateProvider extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
-        /** This is empty because we have no blocks (yet!)
-         *  example:
-         *  blockWithItem(ModBlocks.EXAMPLE);
-         */
+        blockWithItem(ModBlocks.BONE_PILE_BLOCK);
     }
 
     private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
