4ca2534e7ee61b4a1090b755fe338965bb7ba5d6
PriestOfPain <PriestOfPainWasTaken@gmail.com>
2024-03-15 14:24:18 -0700

    docs: add description for TomeOfControlUndeadItem

--- src/main/java/net/priestofpain/lordofthedead/item/custom/TomeOfControlUndeadItem.java
+++ src/main/java/net/priestofpain/lordofthedead/item/custom/TomeOfControlUndeadItem.java
@@ -2,6 +2,10 @@ package net.priestofpain.lordofthedead.item.custom;
 
 import net.minecraft.world.item.Item;
 
+/**
+ * This item can be used to turn an existing undead entity
+ * into an ally of the player. 
+ */
 public class TomeOfControlUndeadItem extends Item {
     public TomeOfControlUndeadItem(Properties pProperties) {
         super(pProperties);
