c228f644957adb51a87e81ecd8f6a823d659df79
PriestOfPain <PriestOfPainWasTaken@gmail.com>
2024-03-15 16:10:33 -0700

    feat: add tooltip for necromancer staff

--- src/main/java/net/priestofpain/lordofthedead/item/custom/NecromancerStaffItem.java
+++ src/main/java/net/priestofpain/lordofthedead/item/custom/NecromancerStaffItem.java
@@ -2,19 +2,25 @@ package net.priestofpain.lordofthedead.item.custom;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.wrapper.PlayerMainInvWrapper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class NecromancerStaffItem extends Item {
 
@@ -60,5 +66,11 @@ public class NecromancerStaffItem extends Item {
             }
         }
         return InteractionResult.FAIL;
+    }
+    // In game tooltip
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("tooltip.lord_of_the_dead.necromancer_staff.tooltip"));
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 }
