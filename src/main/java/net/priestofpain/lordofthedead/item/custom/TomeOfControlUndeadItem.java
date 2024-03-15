cdc13d75fefc467985ab9413220d429561d03d81
PriestOfPain <PriestOfPainWasTaken@gmail.com>
2024-03-15 14:21:02 -0700

    feat: add tome of control undead class

---
+++ src/main/java/net/priestofpain/lordofthedead/item/custom/TomeOfControlUndeadItem.java
@@ -0,0 +1,9 @@
+package net.priestofpain.lordofthedead.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class TomeOfControlUndeadItem extends Item {
+    public TomeOfControlUndeadItem(Properties pProperties) {
+        super(pProperties);
+    }
+}
